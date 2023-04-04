c8e70add13f3dd94f6bc5f7d9a518e41df4777c5
ho <hozheheng@gmail.com>
2023-04-04 14:53:39 +0800

    change 1

---
+++ exercise2.js
@@ -0,0 +1,34 @@
+let dbmongoUsers = [
+    {
+        username : "ho",
+        password : 1234,
+        name : "HO",
+        email : "ho@utem.student.edu.my"
+    },
+    {
+        username : "khoo",
+        password : 12345,
+        name : "KHOO",
+        email : "khoo@utem.student.edu.my"
+
+    }
+]
+
+function login(reqUsername, reqPassword){
+    let matchuser = dbmongoUsers.find(user => user.username == reqUsername)
+    if (!matchuser) return "User not found!"
+
+    
+    if (matchuser.password == reqPassword){
+        return matchuser
+    }
+    else {
+        return "Invalid password!"
+    }
+    //console.log(matchpass)
+
+}
+
+
+//login("khoo", "password")
+console.log(login("ho", "1234"))
